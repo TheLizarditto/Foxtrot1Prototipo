112b1325c0795a580c7efbd7d789a9a629993578
TheLizarditto <matias.haron@gmail.com>
2026-09-12 18:12:49 -0300

    Feat: añadi comentarios varios y limitacion de movimiento

--- Scripts/personaje.gd
+++ Scripts/personaje.gd
@@ -6,20 +6,23 @@ extends Node2D
 
 var posicion := Vector2.ZERO
 
+# Inicializa al personaje ubicandolo en el centro del tablero.
 func _ready() -> void:
 	_actualizar_posicion_centro()
 	
-# Las cartas invocaran esta funcion con un vector de direccion.
+# Recibe una direccion de movimiento y actualiza la posicion en la grilla.
 func movimiento(direccion: Vector2i) -> void:
 	posicion += Vector2(direccion)
 	posicion.x = clampi(posicion.x, 0, tablero.columnas - 1)
 	posicion.y = clampi(posicion.y, 0, tablero.filas - 1)
 	_actualizar_sprite()
 	
+# Calcula la celda central del tablero y mueve ahi al personaje.
 func _actualizar_posicion_centro() -> void:
 	posicion = tablero.obtener_centro_tablero()
 	_actualizar_sprite()
 
+# Convierte la posicion de grilla del personaje a posicion global.
 func _actualizar_sprite() -> void:
 	global_position = tablero.to_global(
 		tablero._obtener_posicion_en_tablero(int(posicion.x), int(posicion.y))
