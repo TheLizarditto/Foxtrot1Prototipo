05b8c53d0ae7b8c3f1c930e6e408eecaa87583c2
Leorn21 <leonelruiz692@gmail.com>
2026-09-12 00:11:35 -0300

    feat: implementacion movimiento en 8 direcciones

--- Scripts/personaje.gd
+++ Scripts/personaje.gd
@@ -9,8 +9,18 @@ var posicion := Vector2.ZERO
 func _ready() -> void:
 	_actualizar_posicion_centro()
 	
-func _process(_delta: float) -> void:
-	pass
+# Las cartas invocaran esta funcion con un vector de direccion.
+func movimiento(direccion: Vector2i) -> void:
+	posicion += Vector2(direccion)
+	posicion.x = clampi(posicion.x, 0, tablero.columnas - 1)
+	posicion.y = clampi(posicion.y, 0, tablero.filas - 1)
+	_actualizar_sprite()
 	
 func _actualizar_posicion_centro() -> void:
 	posicion = tablero.obtener_centro_tablero()
+	_actualizar_sprite()
+
+func _actualizar_sprite() -> void:
+	global_position = tablero.to_global(
+		tablero._obtener_posicion_en_tablero(int(posicion.x), int(posicion.y))
+	)
